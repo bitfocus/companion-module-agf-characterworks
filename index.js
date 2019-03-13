6ebd50ec2e831562501910251478d7c3711b9924
Eddie Wettach <ewettach@EW4-MacBook-Pro.local>
2019-03-12 22:56:44 -0500

    Version 1.0.1

--- index.js
+++ index.js
@@ -20,75 +20,122 @@ class instance extends instance_skel {
 	actions(system) {
 		var self = this;
 		self.system.emit('instance_actions', self.id, {
-            'characterworks': {
-				label: 'Trigger CharacterWorks',
-				options: [
-					{
-						type: 'dropdown',
-						label: 'Action',
-						id: 'action_dropdown',
-						default: 'play_motions',
-						choices: [
-						  { id: 'play_motions', label: 'play_motions' },
-						  { id: 'stop_motions', label: 'stop_motions' },
-						  { id: 'finish_motions', label: 'finish_motions' }
-						]
-					  },
-					{
-						type:  'textinput',
-						label: 'Motion Name',
-						id:    'motion_name',
-						regex: '/^[A-Za-z0-9_\-]*$/'
-					},
-					{
-						type: 'dropdown',
-						label: 'Channel',
-						id: 'channel_dropdown',
-						default: 'preview',
-						choices: [
-						  { id: 'live1', label: 'live1' },
-						  { id: 'live2', label: 'live2' },
-						  { id: 'preview', label: 'preview' }
-						]
-					  }
-				]
-			}
+            'trigger_cw': {
+							label: 'Trigger CharacterWorks',
+							options: [
+								{
+									type: 'dropdown',
+									label: 'Action',
+									id: 'action_dropdown',
+									default: 'play_motions',
+									choices: [
+											{ id: 'play_motions', label: 'play_motions' },
+											{ id: 'stop_motions', label: 'stop_motions' },
+											{ id: 'finish_motions', label: 'finish_motions' }
+									]
+								},
+								{
+									type:  'textinput',
+									label: 'Motion Name',
+									id:    'motion_name',
+									regex: '/^[A-Za-z0-9_\-]*$/'
+								},
+								{
+									type: 'dropdown',
+									label: 'Channel',
+									id: 'channel_dropdown',
+									default: 'preview',
+									choices: [
+										{ id: 'live1', label: 'live1' },
+										{ id: 'live2', label: 'live2' },
+										{ id: 'preview', label: 'preview' }
+									]
+								}
+							]
+						},
+						'set_text': {
+							label: 'Set Text',
+							options: [
+								{
+									type:  'textinput',
+									label: 'Motion Name',
+									id:    'motion_name',
+									width: 12,
+									regex: '/^[A-Za-z0-9_\-]*$/'
+								},
+								{
+									type:  'textinput',
+									label: 'Text Layer',
+									id:    'text_layer',
+									width: 12,
+									default: 'CW text layer variable to replace'
+								},
+								{
+									type:  'textinput',
+									label: 'Text Value',
+									id:    'text_value',
+									width: 12,
+									default: 'Sample Text'
+								},
+								{
+									type: 'dropdown',
+									label: 'Channel',
+									id: 'channel_dropdown',
+									default: 'preview',
+									choices: [
+										{ id: 'live1', label: 'live1' },
+										{ id: 'live2', label: 'live2' },
+										{ id: 'preview', label: 'preview' }
+									]
+								}
+							]
+						}
 		});
 	}
 
 	action(action) {
 		var self = this;
 		var url = 'http://' + self.config.host + ':' + self.config.port;
-		
-        if(action.action=='characterworks'){
-			// create http request client
-			var request = require("request")
+		// Create http request client
+		var request = require("request")
 
-			// create JSON data to HTTP POST to CW
-			var requestData = {
-				"action": action.options.action_dropdown,
-				"motions": [
-				action.options.motion_name
-				],
-				"channel": action.options.channel_dropdown
-			}
-
-			// Send the request to Characterworks
-			request({
-				url: url,
-				method: "POST",
-				json: requestData
-			}, function (error, response, body) {
+		switch (action.action){
+			case 'trigger_cw':
+					// create JSON data to HTTP POST to CW
+					var requestData = {
+						"action": action.options.action_dropdown,
+						"motions": [
+						action.options.motion_name
+						],
+						"channel": action.options.channel_dropdown
+					}
+			break
+			case "set_text":
+					// create JSON data to HTTP POST to CW
+					var requestData = {
+						"action": 'set_text',
+						"layer": action.options.motion_name + '\\' + action.options.text_layer,
+						"value": action.options.text_value,
+						"channel": action.options.channel_dropdown
+					}
+			break
+		}
+	// Send the request to Characterworks
+		request({
+			url: url,
+			method: "POST",
+			json: requestData
+		}, function (error, response, body) {
 				if (!error && response.statusCode === 200) {
-					console.log(body)
-				}
-				else {
-					self.log("Characterworks Send Error: " + error)
-					console.log("response.statusCode: " + response.statusCode)
-					console.log("response.statusText: " + response.statusText)
-				}
-			})
-        }
+						console.log(body)
+					}
+					else {
+						self.log("Characterworks Send Error: " + error)
+						console.log("response.statusCode: " + response.statusCode)
+						console.log("response.statusText: " + response.statusText)
+					}
+				})
+        
 	}
 	// Web config fields
 	config_fields () {
